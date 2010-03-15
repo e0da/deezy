8618871d4d5f0208309b78cfc8d580b31ab8b42b
justin <devnull@localhost>
2010-03-15 10:32:53 -0700

    Forgot to change ::s back to .s

--- db/migrate/20100312233635_add_ip_as_int.rb
+++ db/migrate/20100312233635_add_ip_as_int.rb
@@ -1,8 +1,8 @@
 class AddIpAsInt < ActiveRecord::Migration
   def self.up
     add_column :entries, :ip_int, 'integer unsigned'
-    Entry::reset_column_information
-    Entry::all.each do |e|
+    Entry.reset_column_information
+    Entry.all.each do |e|
       unless e.ip.blank? 
         e.update_attribute :ip_int, Entry::ip_as_int(e.ip)
       end
