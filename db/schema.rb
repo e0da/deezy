62220df466f875c0b8272283736db166e73734eb
justin <devnull@localhost>
2010-03-15 10:07:47 -0700

    Looks like we DO want to run rake db:migrate before committing. *facepalm*

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091216193535) do
+ActiveRecord::Schema.define(:version => 20100312233635) do
 
   create_table "entries", :force => true do |t|
     t.string   "scope"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20091216193535) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "room"
+    t.integer  "ip_int"
   end
 
 end
