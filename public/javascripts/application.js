56eccf7df71458d30121c71a78dcd116fcc9ea13
justin <devnull@localhost>
2010-04-12 14:31:55 -0700

    This addresses (and I think resolves) #44 and #45.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -267,7 +267,24 @@ var deezy_form_layout = function() {
 var deezy_layout = function() {
 
   // private methods
-  var set_up_note_previews,add_collapse_expand_all_notes_links;
+  var set_up_note_previews,add_collapse_expand_all_notes_links,linkify_notes;
+
+  // Linkify appropriate terms in the notes field
+  linkify_notes = function() {
+    var notes = deezy_layout.notes;
+    notes = notes.concat($$('.note_expanded'));
+    var filters = [];
+    filters.push({re:/#(\S+)/gi,repl:'<a href="https://deezy.education.ucsb.edu:444/entries?search[any_like]=%23$1">#$1</a>'});
+    filters.push({re:/wiki:(\S+)/gi,repl:'<a href="http://wiki.education.ucsb.edu/$1">wiki:$1</a>'});
+    filters.push({re:/rt:(\d+)/gi,repl:'<a href="https://rt.education.ucsb.edu/Ticket/Display.html?id=$1">rt:$1</a>'});
+    filters.each(function(filter) {
+      notes.each(function(note) {
+        note.innerHTML = note.innerHTML.replace(filter.re,filter.repl);
+        console.log(filter.re);
+        console.log(filter.repl);
+      });
+    });
+  };
 
   // Set up note previews that can expand to show the whole note content
   set_up_note_previews = function() {
@@ -286,7 +303,7 @@ var deezy_layout = function() {
       note.update(left);
       note.appendChild(right);
       note.expand = function() {
-        right.update('«');
+        right.update('<«');
         left.addClassName('note_expanded');
         right.stopObserving('click');
         right.observe('click',note.collapse);
@@ -332,6 +349,7 @@ var deezy_layout = function() {
     notes:null,
     init:function() {
       deezy_layout.notes = $$('.notes_col');
+      linkify_notes();
 
       // If there's an 'entries' table, set up expandable note previews
       if ($('entries')) { set_up_note_previews(); }
