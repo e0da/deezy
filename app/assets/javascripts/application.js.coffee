e55a4d46f5fab682da774f8a630bb2266659b749
Justin Force <justin.force@gmail.com>
2012-04-02 12:44:18 -0700

    Build some form validation

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -1,3 +1,24 @@
+package_for_validation = (validation) ->
+  field = $("##{validation.id}")
+  field.data 'sanitize', validation.sanitize
+  field.data 'valid', validation.valid
+  field.data 'help', validation.help
+
+trim = (field) ->
+  field.val(field.val().trim())
+
+sanitize = (field) ->
+  field.val field.data('sanitize').call(field.val()) if field.data('sanitize')
+
+valid = (field) ->
+  if field.data('valid')
+    field.val().match field.data('valid')
+  else
+    true
+
+warn = (field) ->
+  console.error field.data('help') if field.data('help')
+
 $ ->
 
   # form stuff
@@ -33,3 +54,49 @@ $ ->
 
       ip_picker.prepend select
       ip_picker.find('.list').append pools[0].ips
+
+    # form validation
+    #
+    validations = [
+      id: 'host_mac'
+      sanitize: ->
+        this.toLowerCase().replace(/[^a-f\d]/g, '').replace(/(..)(..)(..)(..)(..)(..)/, '$1:$2:$3:$4:$5:$6')
+      valid: /^([a-f\d]{2}:){5}[a-f\d]{2}$/
+      help: "A valid MAC address is 12 hexadecimal (0-9A-F) characters. Case and punctuation don't matter."
+    ,
+      id: 'host_ip'
+      valid: /^(((\d{1,3}).){3}\d{1,3}|)$/
+      help: "You probably want to pick an IP address from the list on the right"
+    ,
+      id: 'host_itgid'
+      valid: /^\d{2}[48]00\d{4}$/
+      help: "An ITG ID is the 2 digit year, 400 or 800, then 4 digits, like 084004432."
+    ,
+      id: 'host_room'
+      sanitize: ->
+        this.toUpperCase()
+      valid: /^\d{4}[A-Z]?$/
+      help: "A room number is four digits (optionally followed by a letter)."
+    ,
+      id: 'host_hostname'
+      sanitize: ->
+        this.toLowerCase()
+      valid: /^[a-z][a-z\d-]+$/
+      help: "A hostname must start with a letter and can contain letters, numbers, and an dash (-)."
+    ,
+      id: 'host_uid'
+      sanitize: ->
+        this.toLowerCase()
+      valid: /^.+$/
+      help: "Enter the user's UID."
+    ,
+      id: 'host_notes'
+    ]
+
+    for validation in validations
+      field = package_for_validation validation
+      field.change ->
+        field = $(this)
+        trim(field)
+        sanitize(field)
+        warn(field) unless valid(field)
--- public/deezy/javascripts/application.js
+++ public/deezy/javascripts/application.js
@@ -2,10 +2,42 @@
 
 // compiled ./app/assets/javascripts/application.js.coffee 
 (function() {
+  var package_for_validation, sanitize, trim, valid, warn;
+
+  package_for_validation = function(validation) {
+    var field;
+    field = $("#" + validation.id);
+    field.data('sanitize', validation.sanitize);
+    field.data('valid', validation.valid);
+    return field.data('help', validation.help);
+  };
+
+  trim = function(field) {
+    return field.val(field.val().trim());
+  };
+
+  sanitize = function(field) {
+    if (field.data('sanitize')) {
+      return field.val(field.data('sanitize').call(field.val()));
+    }
+  };
+
+  valid = function(field) {
+    if (field.data('valid')) {
+      return field.val().match(field.data('valid'));
+    } else {
+      return true;
+    }
+  };
+
+  warn = function(field) {
+    if (field.data('help')) return console.error(field.data('help'));
+  };
 
   $(function() {
+    var field, validation, validations, _i, _len, _results;
     if ($('#ip_picker').length > 0) {
-      return $.getJSON('/deezy/freeips.json', function(data) {
+      $.getJSON('/deezy/freeips.json', function(data) {
         var form, ip, ip_picker, li, list, pool, pools, select, _i, _j, _k, _len, _len2, _len3, _ref;
         pools = [];
         ip_picker = $('#ip_picker');
@@ -42,6 +74,59 @@
         ip_picker.prepend(select);
         return ip_picker.find('.list').append(pools[0].ips);
       });
+      validations = [
+        {
+          id: 'host_mac',
+          sanitize: function() {
+            return this.toLowerCase().replace(/[^a-f\d]/g, '').replace(/(..)(..)(..)(..)(..)(..)/, '$1:$2:$3:$4:$5:$6');
+          },
+          valid: /^([a-f\d]{2}:){5}[a-f\d]{2}$/,
+          help: "A valid MAC address is 12 hexadecimal (0-9A-F) characters. Case and punctuation don't matter."
+        }, {
+          id: 'host_ip',
+          valid: /^(((\d{1,3}).){3}\d{1,3}|)$/,
+          help: "You probably want to pick an IP address from the list on the right"
+        }, {
+          id: 'host_itgid',
+          valid: /^\d{2}[48]00\d{4}$/,
+          help: "An ITG ID is the 2 digit year, 400 or 800, then 4 digits, like 084004432."
+        }, {
+          id: 'host_room',
+          sanitize: function() {
+            return this.toUpperCase();
+          },
+          valid: /^\d{4}[A-Z]?$/,
+          help: "A room number is four digits (optionally followed by a letter)."
+        }, {
+          id: 'host_hostname',
+          sanitize: function() {
+            return this.toLowerCase();
+          },
+          valid: /^[a-z][a-z\d-]+$/,
+          help: "A hostname must start with a letter and can contain letters, numbers, and an dash (-)."
+        }, {
+          id: 'host_uid',
+          sanitize: function() {
+            return this.toLowerCase();
+          },
+          valid: /^.+$/,
+          help: "Enter the user's UID."
+        }, {
+          id: 'host_notes'
+        }
+      ];
+      _results = [];
+      for (_i = 0, _len = validations.length; _i < _len; _i++) {
+        validation = validations[_i];
+        field = package_for_validation(validation);
+        _results.push(field.change(function() {
+          field = $(this);
+          trim(field);
+          sanitize(field);
+          if (!valid(field)) return warn(field);
+        }));
+      }
+      return _results;
     }
   });
 
