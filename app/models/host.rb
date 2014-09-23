cc5f5bd41e5ebd3fabf1c8979a60ca9e09df8499
Justin Force <justin.force@gmail.com>
2014-09-23 15:56:38 -0700

    fixed IP validation regex which would not allow blank records

--- app/models/host.rb
+++ app/models/host.rb
@@ -3,7 +3,7 @@ require 'ipaddr'
 class Host < ActiveRecord::Base
   validates_uniqueness_of :hostname
   validates_format_of :mac, :with => /[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}/
-  validates_format_of :ip, :with => /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+  validates_format_of :ip, :with => /(^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$|)/
   validates_format_of :itgid, :with => /\d{2}[48]00\d{4}/
   validates_format_of :hostname, :with => /[a-z0-9]([a-z0-9-][a-z0-9]{0,61}[a-z0-9]|[a-z0-9])/
   validates_format_of :uid, :with => /[a-z0-9]([a-z0-9-][a-z0-9]{0,61}[a-z0-9]|[a-z0-9])/
